952e66e0b5e110ba89ca7bf27802b8144c0ee204
Jeff Klukas <jeff@klukas.net>
2017-09-26 08:01:39 -0400

    -> 0.6.5

--- shiftmanager/metadata.py
+++ shiftmanager/metadata.py
@@ -9,7 +9,7 @@ Information describing the project.
 package = 'shiftmanager'
 project = 'shiftmanager'
 project_no_spaces = project.replace(' ', '')
-version = '0.6.4'
+version = '0.6.5'
 description = 'Management tools for Amazon Redshift'
 authors = ['Jeff Klukas', 'Rob Story', 'Meli Lewis',
            'Allison Keene', 'Xavier Stevens', 'KF Fellows']
@@ -17,5 +17,5 @@ authors_string = ', '.join(authors)
 emails = ['klukas@simple.com', 'allison@simple.com', 'xavier@simple.com',
           'kf@simple.com']
 license = 'BSD'
-copyright = '2016 ' + authors_string
+copyright = '2017 ' + authors_string
 url = 'https://shiftmanager.readthedocs.org'
