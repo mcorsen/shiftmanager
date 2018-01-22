7ddb0da62735c4b2119348d13b53781a6b7df349
andrew <andrew@simple.com>
2018-01-22 09:09:28 -0500

    raise any exception

--- shiftmanager/mixins/postgres.py
+++ shiftmanager/mixins/postgres.py
@@ -200,7 +200,7 @@ libpq-connect.html#LIBPQ-PARAMKEYWORDS
                 # it blocks and no exceptions can reach the main program.
                 s3_thread.join(1)
             s3_keys = s3_thread.s3_keys
-        finally:
+        except:
             s3_thread.abort()
             print("Error while pulling data out of PostgreSQL")
             if cleanup_s3:
@@ -209,6 +209,7 @@ libpq-connect.html#LIBPQ-PARAMKEYWORDS
                     bucket.delete_key(key)
             else:
                 print("Leaving files in place...")
+            raise
 
         print("Uploads all done. Cleaning up temp directory " + tmpdir)
         shutil.rmtree(tmpdir)
@@ -322,12 +323,13 @@ libpq-connect.html#LIBPQ-PARAMKEYWORDS
             try:
                 self.execute(statements)
                 start_idx = end_idx
-            finally:
+            except:
                 # Clean up S3 bucket in the event of any exception
                 if cleanup_s3:
                     print("Error writing to Redshift! Cleaning up S3...")
                     for key in s3_keys:
                         bucket.delete_key(key)
+                raise
 
 
 class S3UploaderThread(Thread):
